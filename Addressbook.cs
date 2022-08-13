c6e11f9aaaa9c9d1bc1ad134f34deab964c54e82
jyoti <jyotidhage10@gmail.com>
2022-08-13 19:03:47 +0530

    [Aaradhya]Add: Delete person using person Name

--- Addressbook.cs
+++ Addressbook.cs
@@ -89,7 +89,18 @@ namespace AddressBook
             }
         
         }
-       
+        public void DeleteContact(string name)
+        {
+            if (addressBook.ContainsKey(name))
+            {
+                addressBook.Remove(name);
+                Console.WriteLine("\nDeleted Succesfully.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nIt Is Not Found.\n");
+            }
+        }
 
 
     }
--- Iperson.cs
+++ Iperson.cs
@@ -11,5 +11,7 @@ namespace AddressBook
         void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
         void EditContact(string name);
 
+        void DeleteContact(string name);
+
     }
 }
